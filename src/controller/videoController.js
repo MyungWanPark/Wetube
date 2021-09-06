a1c2a8544bc99437ecdce8a10bb81356a6587f1f
MyungWanPark <mywanpark@gmail.com>
2021-09-06 21:41:23 +0900

    Add: view & layout.pug

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,8 +1,6 @@
-export const home = (req, res) => res.send("Welcome Home Videos");
-export const see = (req, res) => {
-  return res.send(`you are watching #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("edit videos");
+export const home = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search video");
 export const upload = (req, res) => res.send("upload video");
 export const remove = (req, res) => res.send("remove video");
