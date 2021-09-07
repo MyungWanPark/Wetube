b7e586c147f64d9bd818dbcfcb1cc34b5ddc49fc
MyungWanPark <mywanpark@gmail.com>
2021-09-07 16:39:53 +0900

    Add: use mixins for different data html template

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,4 +1,30 @@
-export const home = (req, res) => res.render("home");
+export const home = (req, res) => {
+  const videos = [
+    {
+      title: "first video",
+      views: 30,
+      comments: 3,
+      like: 1000,
+      share: 10,
+    },
+    {
+      title: "second video",
+      views: 30,
+      comments: 3,
+      like: 1000,
+      share: 10,
+    },
+    {
+      title: "third video",
+      views: 30,
+      comments: 3,
+      like: 1000,
+      share: 10,
+    },
+  ];
+  return res.render("home", { headTitle: "home", videos });
+};
+
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search video");
