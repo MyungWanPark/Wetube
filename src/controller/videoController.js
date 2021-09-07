bf3845c87a15bb467852b73fa2529d50a0f10302
MyungWanPark <mywanpark@gmail.com>
2021-09-07 20:00:27 +0900

    Add: relative address in href

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,31 +1,38 @@
+let videos = [
+  {
+    title: "first video",
+    views: 30,
+    comments: 3,
+    like: 1000,
+    share: 10,
+    id: 1,
+  },
+  {
+    title: "second video",
+    views: 30,
+    comments: 3,
+    like: 1000,
+    share: 10,
+    id: 2,
+  },
+  {
+    title: "third video",
+    views: 30,
+    comments: 3,
+    like: 1000,
+    share: 10,
+    id: 3,
+  },
+];
+
 export const home = (req, res) => {
-  const videos = [
-    {
-      title: "first video",
-      views: 30,
-      comments: 3,
-      like: 1000,
-      share: 10,
-    },
-    {
-      title: "second video",
-      views: 30,
-      comments: 3,
-      like: 1000,
-      share: 10,
-    },
-    {
-      title: "third video",
-      views: 30,
-      comments: 3,
-      like: 1000,
-      share: 10,
-    },
-  ];
   return res.render("home", { headTitle: "home", videos });
 };
-
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { headTitle: `watch ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search video");
 export const upload = (req, res) => res.send("upload video");
