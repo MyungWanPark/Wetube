ff7d433418c994646333178a44dfced9b6f61d1c
MyungWanPark <mywanpark@gmail.com>
2021-09-27 17:21:45 +0900

    Add: add frontend and connect to pug

--- src/server.js
+++ src/server.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use(localMiddleware);
 app.use("/upload", express.static("upload"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
